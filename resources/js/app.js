d3df5b8174adc25ba6c2d6d8c093769a23c91dae
ankur01oct <ankur01oct@gmail.com>
2019-05-03 09:35:49 +0530

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+    // Add minus icon for collapse element which is open by default
+    $(".collapse.in").each(function(){
+        $(this).siblings(".panel-heading").find(".btn-arrow").addClass("rotate");
+    });
+
+    // Toggle plus minus icon on show hide of collapse element
+    $(".collapse").on('show.bs.collapse', function(){
+        $(this).parent().find(".btn-arrow").addClass("rotate");
+    }).on('hide.bs.collapse', function(){
+        $(this).parent().find(".btn-arrow").removeClass("rotate");
+    });
+
+    $(document).on( 'click','[id="btn-yes"]',function(){
+        var value = {};
+        console.log("Yes-Clicked");
+        value.type = "Yes-Clicked"
+        value.position = Number($(this).parent().parent().attr("id").substr(-1)) +1;
+        value.value = {};
+        value.value.topic = $(this).parent().parent().parent().find("h4").first().text().trim();
+        value.value.description = $(this).parent().parent().find("p").first().text();
+        console.log(value);
+        window.parent.postMessage(value,"*");
+        $(this).parent().find('button').hide();
+        $(this).parent().find("p").first().hide();
+        $(this).parent().children().last().removeAttr('hidden');
+
+    })
+
+    $(document).on( 'click','[id="btn-no"]',function(){
+        console.log("No-Clicked");
+        var value = {};
+        value.type = "No-Clicked"
+        value.position = Number($(this).parent().parent().attr("id").substr(-1)) +1;
+        value.value = {};
+        value.value.topic = $(this).parent().parent().parent().find("h4").first().text().trim();
+        value.value.description = $(this).parent().parent().find("p").first().text();
+        console.log(value);
+        window.parent.postMessage(value,"*");
+        $(this).parent().hide();
+        $(this).parent().parent().find('.panel-info').removeAttr('hidden');
+    })
+
+    $(document).on( 'keypress','.input-text',function() {
+        if($(this).val().length > 999) {
+            //display your warinig the way you chose
+            console.log('MaxLength Reached');
+        }
+    });
+    $(document).on( 'submit','#know-more-form',function(e) {
+        e.preventDefault();
+        var inputVal = $( this )[0][0].value; // resolves to current input element.
+        if(inputVal && inputVal.length){
+            var value = {};
+            value.type = "No-Clicked-Know-More-Text"
+            value.position = Number($(this).parent().parent().attr("id").substr(-1)) +1;
+            value.value = {};
+            value.value.topic = $(this).parent().parent().parent().find("h4").first().text().trim();
+            value.value.description = $(this).parent().parent().find("p").first().text();
+            value.value.knowMoreText=inputVal;
+            window.parent.postMessage(value,"*");
+            console.log(value);
+        }
+        $(this).parent().children().last().removeAttr('hidden');
+        $(this).parent().css("padding", "20px");                
+        $(this).parent().css("height", "100px");
+        $(this).parent().children().last().removeAttr('hidden');
+        $(this).parent().children().not('.no-clicked').hide();
+
+
+    });
+    $(document).on('click', '.send-email', function(){
+        var value = {};
+        value.type = "Report-Other-Issues-Clicked";
+        window.parent.postMessage(value,"*");
+        console.log(value);
+        $(this).hide();
+        let parent = $(this).parent();
+        parent.find('.bs-example').hide();
+        parent.find('.info-msg').hide();
+        parent.find('.help-header').hide();
+        parent.find('.help-header-send-email').removeAttr('hidden');
+        parent.find('.info-msg-send-email').removeAttr('hidden');
+        parent.find('.send-email-form').removeAttr('hidden');
+    });
+
+    $(document).on( 'submit','#send-email-form',function(e) {
+        e.preventDefault();
+        var inputVal = $( this )[0][0].value; // resolves to current input element.
+        if(inputVal && inputVal.length){
+            var value = {};
+            value.type = "Initiate-Email-Clicked";
+            value.value = {};
+            value.InitiateEmailBody=inputVal;
+            window.parent.postMessage(value,"*");
+            console.log(value);
+        }
+    });
+    $(document).on( 'keypress','.input-text-form',function() {
+        if($(this).val().length > 999) {
+            //display your warinig the way you chose
+            console.log('MaxLength Reached');
+        }
+    });
+});
---
+++ resources/js/render-from-json.js
@@ -0,0 +1,100 @@
+//$.getJSON("mydata.json", function(data) {
+//    var html = '';
+//    $.each(data, function(key, value){
+//        html += '<div class="dcell">';
+//        html += '<img src="images/'+value.product+'.png"/>';
+//        html += '<label for="'+value.product+'">'+value.name+':</label>';
+//        html += '<input type="text" id="'+value.product+'" name="'+value.product+'" value="0" stock="'+value.stock+'" price="'+value.price+'" required>';
+//        html += '</div>';
+//    });
+//$('#yourContainerId').html(html);
+//});
+var selectedLang = getUrlVars()["selectedlang"];
+$( document ).ready(function() {
+    var jsonUrl;
+    if(selectedLang){
+        jsonUrl = "./resources/res/faq-" + selectedLang.trim() + ".json"
+    }else{
+        jsonUrl="./resources/res/faq-en.json"
+    }
+
+    console.log(jsonUrl);
+    $.getJSON(jsonUrl, function(data) {
+        var html ='';
+        html += '<div class="help-header">'
+            +'<h4>'+data.constants.help+'</h4>'
+            +'<p>'+data.constants.faqMsg+'</p>'
+            +'</div>'
+            +'<div class="help-header-send-email" hidden="true">'
+            +'<h4>'+data.constants.reportIssue+'</h4>'
+            +'<p>'+data.constants.explainMsg+'</p>'
+            +'</div>'
+            +'<div class="info-msg">'
+            +'<p>'+data.constants.resolveMsg+'</p>'
+            +'</div>'
+            +'<div class="info-msg-send-email" hidden="true">'
+            + '<p>'+data.constants.tellMoreMsg+'</p>'
+            + '</div>'
+        $('#header').replaceWith(html);
+        html='';
+        $.each(data.faqs, function(key, value){
+            console.log('value  : ', value, 'key  ', key)
+            html += '<div class="panel panel-default">'
+                +'<a data-toggle="collapse" data-parent="#accordion" href="#collapse'+key+'">'
+                +'<div class="panel-heading" >'
+                +'<h4 class="panel-title">'
+                +value.topic+'<img src="./resources/images/Arrow.png" class="btn-arrow">'
+                +'</h4>'
+                +'</div>'
+                +'</a>'
+                +'<div id="collapse'+key+'" class="panel-collapse collapse">'
+                +'<div class="panel-body">'
+                +'<p>'+value.description+'</p>'
+                +'</div>'
+                +'<div class="panel-interact">'
+                +'<p>'+data.constants.helpMsg+'</p>'
+                +'<button type="button" class="btn" id = "btn-no">'+data.constants.noMsg+'</button>'
+                +'<button type="button" class="btn" style="color: green" id = "btn-yes">'+data.constants.yesMsg+'</button>'
+                +'<p class="yes-clicked" hidden="true">'+data.constants.thanksMsg+'</p>'
+                +'</div>'
+                +'<div class="panel-info" hidden="true">'
+                +'<h6>'+data.constants.sorryMsg+'</h6>'
+                +'<p>'+data.constants.knowMoreMsg+'</p>'
+                +'<form action="#" id="know-more-form"> '
+                +'<textarea type="text" name="moreInfo" placeholder="'+data.constants.typeHere+'" class="input-text"  maxlength="1000"></textarea>'
+                +'<input type="submit" value="'+data.constants.submitButton+'" class="submit-button">'
+                +'</form>'
+                +'<p class="no-clicked" hidden="true">'+data.constants.thanksMsg+'</p>'
+                +'</div>'
+                +'</div>'
+                +'</div>'
+        });
+        $('#accordion').html(html);
+        html='';
+
+        html += '<div class="send-email">'
+            +'<button class="report-button"><img src="./resources/images/Report.png"></span> '+data.constants.reportIssueMsg+'</button>'
+            +'</div>'
+            +'<div class = "send-email-form" hidden="true">'
+            +'<form action="#" id ="send-email-form"> '
+            +'<textarea type="text" name="moreInfo" placeholder="'+data.constants.typeHere+'" class="input-text-form" maxlength="1000"></textarea>'
+                +'<p class = "send-email-info">'+data.constants.triggerEmailMsg+'</p>'
+                +'<input type="submit" value="'+data.constants.initiateEmailButton+'" class="submit-button">'
+                +'</form>'
+                +'</div>'
+        $('#send-email').replaceWith(html);
+    })
+});
+
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
