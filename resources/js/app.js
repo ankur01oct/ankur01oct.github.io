2c9e47b60f98cc952b74128d827c5f935e1815ce
ankur01oct <ankur01oct@gmail.com>
2019-05-04 08:31:38 +0530

    Issue SB-11938 feat:FAQ UI

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,8 @@
 $(document).ready(function(){
-    // Add minus icon for collapse element which is open by default
-    $(".collapse.in").each(function(){
-        $(this).siblings(".panel-heading").find(".btn-arrow").addClass("rotate");
-    });
-
     // Toggle plus minus icon on show hide of collapse element
-    $(".collapse").on('show.bs.collapse', function(){
+     $(document).on('show.bs.collapse', ".collapse", function() {
         $(this).parent().find(".btn-arrow").addClass("rotate");
-    }).on('hide.bs.collapse', function(){
+    }).on('hide.bs.collapse', ".collapse", function() {
         $(this).parent().find(".btn-arrow").removeClass("rotate");
     });
 
